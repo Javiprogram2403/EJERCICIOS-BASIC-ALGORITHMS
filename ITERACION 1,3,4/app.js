d6c01a04394b20444cd03fa9e3c3818653d9b861
Javi <emoayllonjavier@gmail.com>
2024-09-21 16:22:39 +0200

    ITERACION 1,3,4

---
+++ ITERACION 1,3,4/app.js
@@ -0,0 +1,75 @@
+// iteración 1 variables
+
+// 1.1 
+let myFavouriteHero = "Hulk" 
+// 1.2 
+let x = 50 
+// 1.3
+let h = 5 
+let y = 10 
+// 1.4
+let z = "h" + "y"
+
+// iteración 3 Operadores
+
+// 1.1
+console.log(10*5); 
+// 1.2
+console.log(10/2) 
+// 1.3
+console.log(15%9) 
+// 1.4 
+let p = 10 
+let j = 5 
+sum = 10 + 5 
+console.log(sum); 
+// 1.5 
+let c = 10 
+let m = 5 
+multiply = 10*5 
+console.log (multiply) 
+
+// Iteración 4 Arrays
+
+// 1.1 
+const avengers = ["HULK" , "SPIDERMAN" , "BLACK PANTHER"]
+console.log(avengers.shift())
+// 1.2 
+console.log(avengers) 
+let superheroes = ["IRONMAN" , "SPIDERMAN" , "BLACK PANTHER"]
+console.log (superheroes)
+// 1.3 
+let vengadores = ["HULK" , "SPIDERMAN" , "BLACK PANTHER"]
+console.log(vengadores.length);
+
+// 1.4 
+const rickAndMortyCharacters = ["Rick" , "Beth" , "Jerry"]; 
+rickAndMortyCharacters.push ("Morty" , "Summer");
+console.log (rickAndMortyCharacters);
+let ultimoCharacter = rickAndMortyCharacters.pop(); 
+console.log (rickAndMortyCharacters);
+console.log (ultimoCharacter); 
+
+// 1.5 
+rickAndMortyCharacters.push ("Summer" , "Lapiz Lopez") 
+console.log (rickAndMortyCharacters)
+let last = rickAndMortyCharacters.pop(); 
+console.log (rickAndMortyCharacters); 
+console.log (rickAndMortyCharacters.shift())
+console.log (rickAndMortyCharacters) 
+let coleccion = [
+"Rick",
+"Beth",
+"Jerry",
+"Morty" ,
+"Summer",
+"Lapiz Lopez",
+]
+console.log (coleccion [0]) 
+console.log (coleccion [5])
+
+// 1.6 
+let famousCharacters = ["Rick" , "Beth" , "Jerry" , "Morty" , "Summer" , "Lapiz Lopez"]
+console.log (famousCharacters)
+delete(famousCharacters [1])
+console.log(famousCharacters); 
